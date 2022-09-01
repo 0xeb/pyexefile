9ba8df1971f8f0ce578a83dd6a499e96c920f404
Elias Bachaalany <elias.bachaalany@gmail.com>
2022-08-31 21:44:53 -0700

    made as a package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='exefile',
+    version='0.0.1',
+    author='Elias Bachaalany',
+    author_email='elias.bachaalany@gmail.com',
+    packages=['exefile'],
+    scripts=[],
+    url='',
+    license='LICENSE',
+    description='A thin wrapper on top of various executable parsers such as pefile, and others.',
+)
